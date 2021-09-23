5cbc4dab83e27a02908f5497df14cae8ecf7396f
Mamadou Ben TRAORE <tmadben@gmail.com>
2021-09-23 09:35:00 +0000

    First Commit
    First Commit to init the repository

---
+++ tmbArrayTools.js
@@ -0,0 +1,28 @@
+/**
+ * FUNCTION NAME: getDistinctArrayElements
+ * 
+ * AUTHOR: Mamadou Ben TRAORE
+ * 
+ * CREATION DATE: 09222021
+ * 
+ * LAST MODIFICATION DATE: 09222021
+ * 
+ * VERSION: 1.0.0 .
+ * 
+ * 
+ * DESCRIPTION: This function has an Array (A) as input and will return another array with only the first occurence of each element of A.
+ * getDistinctArrayElements will return false if the input parameter is not an array.
+ * getDistinctArrayElements will return [undefined,0] for [,0] as input parameter.
+ * 
+*/
+function getDistinctArrayElements(A){
+    if(Array.isArray(A)){
+        var distinctElementArray = [];
+        for(var i=0; i<A.length; i++){
+            if(distinctElementArray.indexOf(A[i]) == -1){
+                distinctElementArray.push(A[i]);
+            }
+        }
+        return distinctElementArray;  
+    } else return false;   
+}
